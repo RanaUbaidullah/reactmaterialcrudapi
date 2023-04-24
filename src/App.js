904c5fe93cb6aabbc32e8347a8b3d726ff1ee841
Heart <researchfigma@gmail.com>
2023-04-24 16:04:55 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import View from "./components/student/View";
 import Edit from "./components/student/Edit";
+
+                                                        // json-server --watch db.json -p3232
+
 function App() {
   return (
     <>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,139 +1,161 @@
- import { Typography, Box, makeStyles, Grid, TextField, Button} from "@material-ui/core"
+import {
+  Typography,
+  Box,
+  makeStyles,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
 
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
-import { deepPurple, green } from '@material-ui/core/colors';  
-import List from "../student/List"; 
-import axios from "axios"; 
-import { useState } from "react"; 
-const useStyles = makeStyles({ 
- headingColor: { 
-  backgroundColor: deepPurple[400], 
-  color: "white" 
- }, 
- addStuColor: { 
-  backgroundColor: green[400],  
-  color: "white" 
- },  
-}) 
- 
+import { deepPurple, green } from "@material-ui/core/colors";
+import List from "../student/List";
+import axios from "axios";
+import { useState } from "react";
+const useStyles = makeStyles({
+  headingColor: {
+    backgroundColor: deepPurple[400],
+    color: "white",
+  },
+  addStuColor: {
+    backgroundColor: green[400],
+    color: "white",
+  },
+});
+
 const Home = () => {
-  const [value, setValue] = useState(
-    {
-      stuname:"",
-      email:""
-    }
-  );
+  const [value, setValue] = useState({
+    stuname: "",
+    email: "",
+  });
   var num = 0;
-   const [open, setOpen] = useState(false);
-  
- const classes = useStyles(); 
- const [student, setStudent] = useState({ 
-  stuname: "", 
-  email: "" 
- }); 
- const [status, setStatus] = useState(); 
- 
- function onTextFieldChange(e) { 
-  setStudent({ 
-   ...student, 
-   [e.target.name]: e.target.value 
-  })
-  setValue({...value,[e.target.name]: e.target.value} )
- } 
- const handleClickOpen = (e) => {
-e.preventDefault()
-   setOpen(true);
-   if(value.email===""){
-    num = 1
-   }
-   if(value.stuname===""){
-    num = 1
-   }
-   if(num){    
-    setOpen(false)
-   }
- };
+  const [open, setOpen] = useState(false);
 
- const handleClose = () => {
-   setOpen(false);
- };
- async function onFormSubmit(e) {
+  const classes = useStyles();
+  const [student, setStudent] = useState({
+    stuname: "",
+    email: "",
+  });
+  const [status, setStatus] = useState();
 
- 
-      e.preventDefault() 
-      try { 
-       await axios.post(`http://localhost:3232/students`, student) 
-       setStatus(true); 
-       setOpen(false)
-      } catch (error) { 
-       console.log("Something is Wrong"); 
-      }
-   
- } 
- if (status) { 
-  return <Home /> 
- } 
- return (  
-  <> 
-  {/* dialog box */}
-  <Dialog
-       
-        open={open}
-       
-        aria-labelledby="responsive-dialog-title"
-      >
+  function onTextFieldChange(e) {
+    setStudent({
+      ...student,
+      [e.target.name]: e.target.value,
+    });
+    setValue({ ...value, [e.target.name]: e.target.value });
+  }
+  const handleClickOpen = (e) => {
+    e.preventDefault();
+    setOpen(true);
+    if (value.email === "") {
+      num = 1;
+    }
+    if (value.stuname === "") {
+      num = 1;
+    }
+    if (num) {
+      setOpen(false);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  async function onFormSubmit(e) {
+    e.preventDefault();
+    try {
+      await axios.post(`http://localhost:3232/students`, student);
+      setStatus(true);
+      setOpen(false);
+    } catch (error) {
+      console.log("Something is Wrong");
+    }
+  }
+  if (status) {
+    return <Home />;
+  }
+  return (
+    <>
+      {/* dialog box */}
+      <Dialog open={open} aria-labelledby="responsive-dialog-title">
         <DialogTitle id="responsive-dialog-title">
           {"Click ok to add the file!"}
         </DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            Aur You sure to add this
-          </DialogContentText>
-        </DialogContent> 
+          <DialogContentText>Aur You sure to add this</DialogContentText>
+        </DialogContent>
         <DialogActions>
           <Button autoFocus onClick={handleClose}>
             Cancel
           </Button>
-          <Button  autoFocus onClick={e => onFormSubmit(e)}>
+          <Button autoFocus onClick={(e) => onFormSubmit(e)}>
             Add
           </Button>
         </DialogActions>
       </Dialog>
-   <Box textAlign="center" className={classes.headingColor} p={2} mb={2}>
- <Typography variant="h2">Ubaidullah</Typography>
-    </Box>
- <Grid container justify="center" spacing={4}>
-     <Grid item md={6} xs={12}>
-      <Box textAlign="center" p={2} className={classes.addStuColor} mb={2}>
- <Typography variant="h4">Add Student</Typography>
+      <Box textAlign="center" className={classes.headingColor} p={2} mb={2}>
+        <Typography variant="h2">Ubaidullah</Typography>
       </Box>
-      <form noValidate>
-       <Grid container spacing={2}>
-        <Grid item xs={12}>
-         <TextField autoComplete="stuname" error={!value.stuname} name="stuname" variant="outlined" required fullWidth id="stuname" label="Name" onChange={e => onTextFieldChange(e)}
-         />
- </Grid>
-        <Grid item xs={12}>
-         <TextField autoComplete="email" error={!value.email}  name="email" variant="outlined" required fullWidth id="email" label="Email Address" onChange={e => onTextFieldChange(e)} />
+      <Grid container justify="center" spacing={4}>
+        <Grid item md={6} xs={12}>
+          <Box textAlign="center" p={2} className={classes.addStuColor} mb={2}>
+            <Typography variant="h4">Add Student</Typography>
+          </Box>
+          <form noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="stuname"
+                  error={!value.stuname}
+                  name="stuname"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="stuname"
+                  label="Name"
+                  onChange={(e) => onTextFieldChange(e)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="email"
+                  error={!value.email}
+                  name="email"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  onChange={(e) => onTextFieldChange(e)}
+                />
+              </Grid>
+            </Grid>
+            <Box m={3}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                onClick={(e) => handleClickOpen(e)}
+              >
+                Add
+              </Button>
+            </Box>
+          </form>
         </Grid>
-       </Grid>
-       <Box m={3}>
- <Button type="submit" variant="contained" color="primary" fullWidth onClick={e=>handleClickOpen(e)}>Add</Button>
- </Box>
-       </form>
-     </Grid>
 
-     <Grid item md={6} xs={12}>
-      <List />
-     </Grid>
-    </Grid>
- </>
-  )
- }
- 
- export default Home
+        <Grid item md={6} xs={12}>
+          <List />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Home;
